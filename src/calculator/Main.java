f3bbb024075728961cb431867036facd39439be3
黒澤央弥 <nn.kurosawahiroya@gamil.com>
2019-12-17 16:02:43 +0900

    コード最適化

--- src/calculator/Main.java
+++
@@ -1,9 +0,0 @@
-package calculator;
-
-public class Main {
-	public static void main(String args[])
-	{
-		MyFrame frame = new MyFrame("電卓");
-		frame.setVisible(true);
-	}
-}
---
+++ src/com/Calculator/Main.java
@@ -0,0 +1,8 @@
+package com.Calculator;
+
+public class Main {
+    public static void main(String args[]) {
+        MyFrame frame = new MyFrame("電卓");
+        frame.setVisible(true);
+    }
+}
